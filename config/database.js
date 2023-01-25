0f5d18f4beea1ad7db823e7cde7c3c850fb4e3ff
Diana Cuenca <diana.cuenca.zambrano@udla.edu.ec.com>
2023-01-24 22:34:25 -0500

    dotenv

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
+require('dotenv').config()
    
 // create the connection to database
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'admin',
-  database: 'iwdcminicore'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
   
 module.exports = db;
